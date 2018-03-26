c8481e42ea05b8d64857a434d892909b1c73911e
Jhoan Manuel Muñoz Serrano <jhoanmanuelms@gmail.com>
2018-03-26 18:09:11 -0500

    - Arrays.sort approach - Add test data

--- DataStructures/8-RunningMedian/RunningMedian.java
+++ DataStructures/8-RunningMedian/RunningMedian.java
@@ -1,35 +1,33 @@
 import utils.Utils;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
-class MinHeap {
-  List<Integer> items = new ArrayList<>();
+class MediumsHeap {
+  private int size = 0;
+  private int[] items;
 
-  public void add(Integer value) {
-    for (int index = 0; index < items.size(); index++) {
-      if (value < items.get(index)) {
-        items.add(index, value);
-        return;
-      }
-    }
-
-    items.add(value);
+  public void add(int item) {
+    items[size] = item;
+    size++;
+    Arrays.parallelSort(items, 0, size);
   }
 
+
   public double calculateMedian() {
-    int middle = items.size() / 2;
-    if (items.size() % 2 == 0) {
-      return ((double) (items.get(middle) + items.get(middle - 1)) / 2);
+    int middle = size / 2;
+    if (size % 2 == 0) {
+      return ((double) (items[middle] + items[middle - 1]) / 2);
     }
 
-    return items.get(middle);
+    return items[middle];
   }
 }
 
 public class RunningMedian {
   public static void main(String args[]) {
-    MinHeap heap = new MinHeap();
+    MediumsHeap heap = new MediumsHeap();
     List<String> testData = Utils.readLines("TestData8/input01.txt");
     List<String> expectedResults = Utils.readLines("TestData8/output01.txt");
     List<String> results = new ArrayList<>();
