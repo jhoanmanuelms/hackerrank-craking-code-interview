ddfeef69f07044f19eecb30ec0086aa74bf34f18
Jhoan Manuel Muñoz Serrano <jhoanmanuelms@gmail.com>
2018-03-25 20:39:36 -0500

    TreeSet approach for RunningMedian problem

--- DataStructures/8-RunningMedian/RunningMedian.java
+++ DataStructures/8-RunningMedian/RunningMedian.java
@@ -1,68 +1,44 @@
-import java.util.Comparator;
 import java.util.Iterator;
-import java.util.PriorityQueue;
+import java.util.Set;
+import java.util.TreeSet;
 
 public class RunningMedian {
-  private static Comparator<Integer> comparator = new Comparator<Integer>() {
-    @Override
-    public int compare(Integer o1, Integer o2) {
-      if (o1 < o2) return -1;
-      if (o1 > o2) return 1;
-      return 0;
-    }
-  };
-
-  private static PriorityQueue<Integer> heap = new PriorityQueue<>(comparator);
+  private static Set<Integer> heap = new TreeSet<>();
 
   private static Double addValue(Integer value) {
     heap.add(value);
-    Integer[] values = new Integer[heap.size()];
-    heap.toArray(values);
+    int index = 0;
+    int size = heap.size();
+    int middle = (int)Math.floor(size / 2);
+    Iterator<Integer> iterator = heap.iterator();
 
-    int size = values.length;
-    int middle = size / 2;
-
-    if (size % 2 == 0) {
-
-      return Double.valueOf((values[middle] + values[middle - 1]) / 2);
+    if (heap.size() == 1) {
+      return Double.valueOf(value);
     }
 
-    return Double.valueOf(values[(int)Math.floor(middle)]);
+    while(index < middle - 1) {
+      iterator.next();
+      index++;
+    }
+
+    if (size % 2 == 0) {
+      int middleValue = iterator.next();
+      int nextToMiddleValue = iterator.next();
+
+      return Double.valueOf((double)(middleValue + nextToMiddleValue) / 2);
+    }
+
+    int skipped = iterator.next();
+    int median = iterator.next();
+    return Double.valueOf(median);
   }
 
   public static void main(String args[]) {
-    heap.add(12);
-    heap.add(4);
-    heap.add(5);
-    heap.add(3);
-    heap.add(8);
-    heap.add(7);
-//    System.out.println(addValue(12));
-//    System.out.println(addValue(4));
-//    System.out.println(addValue(5));
-//    System.out.println(addValue(3));
-//    System.out.println(addValue(8));
-//    System.out.println(addValue(7));
-//    heap.add(10);
-//    heap.add(2);
-//
-//    Iterator<Integer> heapIterator = heap.iterator();
-//    while (heapIterator.hasNext()) {
-//      System.out.println(heapIterator.next());
-//    }
-//
-//    heap.add(1);
-//    heap.add(4);
-//
-//    heapIterator = heap.iterator();
-//    while (heapIterator.hasNext()) {
-//      System.out.println(heapIterator.next());
-//    }
-//
-    Integer data;
-    do {
-      data = heap.poll();
-      System.out.println(data);
-    }while (data != null);
+    System.out.println(addValue(12));
+    System.out.println(addValue(4));
+    System.out.println(addValue(5));
+    System.out.println(addValue(3));
+    System.out.println(addValue(8));
+    System.out.println(addValue(7));
   }
 }
