1a5049455fd5235494e13efdc41042068418b257
Jhoan Manuel Muñoz Serrano <jhoanmanuelms@gmail.com>
2018-03-25 21:26:52 -0500

    Hackerrank suggested Heap (GG)

--- DataStructures/8-RunningMedian/RunningMedian.java
+++ DataStructures/8-RunningMedian/RunningMedian.java
@@ -1,48 +1,75 @@
 import utils.Utils;
 
 import java.util.ArrayList;
-import java.util.Iterator;
+import java.util.Arrays;
 import java.util.List;
-import java.util.Set;
-import java.util.TreeSet;
 
-public class RunningMedian {
-  private static Set<Integer> heap = new TreeSet<>();
+class MinHeap {
+  private int capacity = 10;
+  private int size = 0;
+  private int[] items = new int[capacity];
 
-  private static Double addValue(Integer value) {
-    heap.add(value);
-    int index = 0;
-    int size = heap.size();
-    int middle = (int)Math.floor(size / 2);
-    Iterator<Integer> iterator = heap.iterator();
-
-    if (heap.size() == 1) {
-      return Double.valueOf(value);
-    }
-
-    while(index < middle - 1) {
-      iterator.next();
-      index++;
-    }
-
-    if (size % 2 == 0) {
-      int middleValue = iterator.next();
-      int nextToMiddleValue = iterator.next();
-
-      return Double.valueOf((double)(middleValue + nextToMiddleValue) / 2);
-    }
-
-    int skipped = iterator.next();
-    int median = iterator.next();
-    return Double.valueOf(median);
+  public void add(int item) {
+    ensureExtraCapacity();
+    items[size] = item;
+    size++;
+    heapifyUp();
   }
 
+  public double calculateMedian() {
+    int middle = size / 2;
+    if (size % 2 == 0) {
+      return ((double)(items[middle] + items[middle - 1]) / 2);
+    }
+
+    return items[middle];
+  }
+
+  private void heapifyUp() {
+    int index = size - 1;
+    while (hasParent(index) && parent(index) > items[index]) {
+      swap(getParentIndex(index), index);
+      index = getParentIndex(index);
+    }
+  }
+
+  private int getParentIndex(int childIndex) {
+    return (childIndex - 1) / 2;
+  }
+
+  private boolean hasParent(int index) {
+    return getParentIndex(index) >= 0;
+  }
+
+  private int parent(int index) {
+    return items[getParentIndex(index)];
+  }
+
+  private void swap(int indexOne, int indexTwo) {
+    int temp = items[indexOne];
+    items[indexOne] = items[indexTwo];
+    items[indexTwo] = temp;
+  }
+
+  private void ensureExtraCapacity() {
+    if (size == capacity) {
+      items = Arrays.copyOf(items, capacity * 2);
+      capacity *= 2;
+    }
+  }
+}
+
+public class RunningMedian {
   public static void main(String args[]) {
+    MinHeap heap = new MinHeap();
     List<String> testData = Utils.readLines("TestData8/input00.txt");
     List<String> expectedResults = Utils.readLines("TestData8/output00.txt");
     List<String> results = new ArrayList<>();
 
-    testData.stream().forEach(value -> results.add(String.valueOf(addValue(Integer.valueOf(value)))));
+    testData.stream().forEach(value -> {
+      heap.add(Integer.valueOf(value));
+      results.add(String.valueOf(heap.calculateMedian()));
+    });
     Utils.assertResults(testData, expectedResults, results);
   }
 }
