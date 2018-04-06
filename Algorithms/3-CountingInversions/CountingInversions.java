9ac888dff71d9504daa2a2d30c25b382dd67859d
Jhoan Manuel Muñoz Serrano <jhoanmanuelms@gmail.com>
2018-04-06 11:12:51 -0500

    Improve variable naming

--- Algorithms/3-CountingInversions/CountingInversions.java
+++ Algorithms/3-CountingInversions/CountingInversions.java
@@ -10,7 +10,7 @@ public class CountingInversions {
     }
 
     // Recursive Case
-    int middle = size >> 1;
+    int middle = size / 2;
     int[] left = Arrays.copyOfRange(array, 0, middle);
     int[] right = Arrays.copyOfRange(array, middle, array.length);
     long inversions = countInversions(left) + countInversions(right);
@@ -18,18 +18,18 @@ public class CountingInversions {
     int range = size - middle;
     int leftIndex = 0;
     int rightIndex = 0;
-    for(int i = 0; i < size; i++) {
+    for(int index = 0; index < size; index++) {
       if(
           leftIndex < middle
               && (
               rightIndex >= range || left[leftIndex] <= right[rightIndex]
           )
           ) {
-        array[i] = left[leftIndex++];
+        array[index] = left[leftIndex++];
         inversions += rightIndex;
       }
       else if(rightIndex < range) {
-        array[i] = right[rightIndex++];
+        array[index] = right[rightIndex++];
       }
     }
 
@@ -45,15 +45,15 @@ public class CountingInversions {
 
   public static void main(String args[]) {
     int[] testArray = { 2, 1, 3, 1, 2 };
-    printArray(testArray);
     System.out.println("Inversions -> " + countInversions(testArray));
+    printArray(testArray);
 
     int[] testArray2 = { 1, 1, 1, 2, 2 };
-    printArray(testArray2);
     System.out.println("Inversions -> " + countInversions(testArray2));
+    printArray(testArray2);
 
     int[] testArray3 = { 14, 7, 23, 72, 1, 11, 9, 2 };
-    printArray(testArray3);
     System.out.println("Inversions -> " + countInversions(testArray3));
+    printArray(testArray3);
   }
 }
